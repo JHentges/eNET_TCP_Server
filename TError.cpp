9ef9088eab865b5bd9eba568e84c2171f6873fc7
Paras Bhanot <parasbhanot@happiestminds.com>
2022-08-18 23:35:37 +0530

    TMessage::parsePayload wasn't handling 0-data DIds

--- TError.cpp
+++ TError.cpp
@@ -1,5 +1,9 @@
+#include <memory>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
 #include "TError.h"
-
 
 const char *err_msg[] = {
 	/*   0 */ "VALID",
@@ -20,3 +24,35 @@ const char *err_msg[] = {
 	/* -15 */ "ADC FATAL",
 };
 
+
+int Error(const std::string message, const source_location &loc)
+{
+	std::cout << message << std::endl;
+	logging::log::emit<logging::Error>() << loc.file_name() << " : " << loc.function_name() << "(" << loc.line() << ")" << message << logging::log::endl;
+	return 0;
+}
+
+
+int TraceBytes(const std::string intro, const TBytes bytes, bool crlf, const source_location &loc)
+{
+	std::stringstream msg;
+	msg << intro;
+	for (auto byt : bytes)
+		msg << std::hex << std::setfill('0') << std::setw(2) << std::uppercase << static_cast<int>(byt) << " ";
+	if (crlf)
+		msg << std::endl;
+	logging::log::emit<logging::Trace>() << loc.file_name() << " : " << loc.function_name() << "(" << loc.line() << ")" << msg.str() << logging::log::endl;
+	return 0;
+}
+
+int ErrorBytes(const std::string intro, const TBytes bytes, bool crlf, const source_location &loc)
+{
+	std::stringstream msg;
+	msg << intro;
+	for (auto byt : bytes)
+		msg << std::hex << std::setfill('0') << std::setw(2) << std::uppercase << static_cast<int>(byt) << " ";
+	if (crlf)
+		msg << std::endl;
+	logging::log::emit<logging::Error>() << loc.file_name() << " : " << loc.function_name() << "(" << loc.line() << ")" << msg.str() << logging::log::endl;
+	return 0;
+}
